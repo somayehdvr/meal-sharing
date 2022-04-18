f7a58d375b802e81ed57fd4f08e0c09fe3a618fe
somayehdvr <somayehdvr@gmail.com>
2022-04-18 16:38:10 +0200

    nodejs week3 meal-sharing

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,11 +2,126 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
+// Returns meals // GET api/meals/?
 router.get("/", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    // knex syntax for selecting things
+    const meals = await knex("meals").select("*");
+    const maxPrice = request.query.maxPrice
+    const availableReservations = request.params.availableReservations
+    const title = request.query.title
+    const createdAfter = request.query.createdAfter
+    const limit = request.query.limit
+    // Only specific number of meals with a specific max price // api/meals?limit=4&maxPrice=90
+    if (limit && maxPrice) {
+      if (!Number.isInteger(parseInt(limit)) || !Number.isInteger(parseInt(maxPrice))) {
+        response.statusCode = 400;
+        response.json({ "message": "bad input" });
+        return;
+      } else {
+        const meal = await knex("meals").select("*").where('price', '<', maxPrice).limit(limit);
+        response.send(meal)
+      }
+    // Get meals that has a price smaller than maxPrice // api/meals?maxPrice=90
+    } else if (maxPrice) {
+      if (!Number.isInteger(parseInt(maxPrice))) {
+        response.statusCode = 400;
+        response.json({ "message": "bad input" });
+        return;
+      } else {
+        const meal = await knex("meals").select("*").where('price', '<', maxPrice);
+        response.json(meal)
+      }
+      // Get meals that still has available reservations // api/meals?availableReservations=true
+    } else if (availableReservations) {
+      const meal = await knex("meals").select("*").where('max_reservations', '>' , '0');
+      response.json(meal)
+      // Get meals that partially match a title // api/meals?title=Indian%20platter
+    } else if (title) {
+      const meal = await knex("meals").select("*").where({ title: title });
+      response.json(meal)
+    // Get meals that has been created after the date // api/meals?createdAfter=2019-04-05
+    } else if (createdAfter) {
+      if (!Date.parse(createdAfter)) {
+        response.statusCode = 400;
+        response.json({ "message": "bad input" });
+        return;
+      } else {
+        const meal = await knex("meals").select("*").where('created_date', '>', createdAfter);
+        response.json(meal)
+      }
+    // Only specific number of meals // api/meals?limit=4
+    } else if (limit) {
+      if (!Number.isInteger(parseInt(limit))) {
+        response.statusCode = 400;
+        response.json({ "message": "bad input" });
+        return;
+      } else {
+        const meal = await knex("meals").select("*").limit(limit);
+        response.send(meal)
+      }
+    // Returns all meals // GET api/meals/
+    } else {
+      response.send(meals);
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+//Adds a new meal // POST api/meals/
+router.post("/", async (request, response) => {
+  try {
+    const body = request.body;
+    if (Object.keys(body).length === 0 || body === {}) {
+      response.statusCode = 422;
+      response.json({ "message": "bad input" });
+      return;
+    }
+    console.log(body)
+    const meal = await knex("meals").insert({
+      title: body.title,
+      description: body.description,
+      location: body.location,
+      when: new Date(),
+      max_reservations: body.max_reservations,
+      price: body.price,
+      created_date: new Date()
+    });
+    response.json(meal)
+  } catch (err) {
+    throw err;
+  }
+});
+
+// Returns meal by id // GET api/meals/2
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const meal = await knex("meals").select("*").where({ id: id });
+    res.json(meal);
+  } catch (err) {
+    throw err;
+  }
+});
+
+// Updates the meal by id // PUT api/meals/2
+router.put("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const meal = await knex("meals").update({ title: "new title" }).where({ id: id });
+    response.json(meal)
+  } catch (error) {
+    throw error;
+  }
+});
+
+// Deletes the meal by id	// DELETE api/meals/2
+router.delete("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const meal = await knex("meals").del().where({ id: id });
+    response.json(meal)
   } catch (error) {
     throw error;
   }
---
+++ src/backend/api/reservations.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../database");
+
+// Returns all reservations // GET api/reservations/
+router.get("/", async (request, response) => {
+    try {
+      // knex syntax for selecting things
+        const reservations = await knex("reservations").select("*");
+        response.send(reservations);
+    } catch (error) {
+    throw error;
+  ``}
+});
+
+// Adds a new reservation // POST api/reservations/
+router.post("/", async (request, response) => {
+    try {
+      const body = request.body;
+      if (Object.keys(body).length === 0 || body === {}) {
+        response.statusCode = 422;
+        response.json({ "message": "bad input" });
+        return;
+      }
+      console.log(body)
+      const reservation = await knex("reservations").insert({
+        number_of_guests: body.number_of_guests,
+        meal_id: body.meal_id,
+        created_date: new Date(),
+        contact_phonenumber: body.contact_phonenumber,
+        contact_name: body.contact_name,
+        contact_email: body.contact_email
+      });
+      response.json(reservation)
+    } catch (err) {
+      throw err;
+    }
+  });
+
+// Returns reservation by id // GET api/reservations/2
+router.get("/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const reservation = await knex("reservations").select("*").where({ id: id });
+      res.json(reservation);
+    } catch (err) {
+      throw err;
+    }
+});
+  
+// Updates the reservation by id // PUT api/reservations/2
+router.put("/:id", async (request, response) => {
+    try {
+      const id = request.params.id;
+      const reservation = await knex("reservations").update({ contact_name: "new contact_name" }).where({ id: id });
+      response.json(reservation)
+    } catch (error) {
+      throw error;
+    }
+});
+  
+// Deletes the reservation by id // DELETE api/reservations/2
+router.delete("/:id", async (request, response) => {
+    try {
+      const id = request.params.id;
+      const reservation = await knex("reservations").del().where({ id: id });
+      response.json(reservation)
+    } catch (error) {
+      throw error;
+    }
+  });
+
+module.exports = router;
