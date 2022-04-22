5b2de17bbd28228980f8c743e0f9b538da31db58
somayehdvr <somayehdvr@gmail.com>
2022-04-23 00:00:42 +0200

    comments applied

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -6,98 +6,45 @@ const knex = require("../database");
 router.get("/", async (request, response) => {
   try {
     // knex syntax for selecting things
-    let meals = await knex("meals").select("*");
-    const maxPrice = request.query.maxPrice
-    const availableReservations = request.params.availableReservations
-    const title = request.query.title
-    const createdAfter = request.query.createdAfter
-    const limit = request.query.limit
-    if (maxPrice) {
-      if (!Number.isInteger(parseInt(maxPrice))) {
-        response.statusCode = 400;
-        response.json({ "message": "bad input" });
-        return;
-      } else {
-        meals = meals.filter(meal => parseInt(meal.price) < maxPrice)
+    let meals = await getMeals(request.query);
+    function getMeals(query) {
+      const meals = knex("meals")
+      if ("maxPrice" in query) {
+        if (!Number.isInteger(parseInt(query.maxPrice))) {
+          response.statusCode = 400;
+          response.json({ "message": "bad input" });
+          return;
+        } else {
+          meals.where('price', '<', query.maxPrice)
+        }
       }
-    }
-    if (availableReservations) {
-      meals = meals.filter(meal => meal.max_reservations < 0)
-    }
-    if (title) {
-      meals = meals.filter(meal => meal.title.includes(title))
-    }
-    if (createdAfter) {
-      if (!Date.parse(createdAfter)) {
-        response.statusCode = 400;
-        response.json({ "message": "bad input" });
-        return;
-      } else {
-        meals = meals.filter(meal => Date.parse(meal.createdAt) >= Date.parse(createdAfter))
+      if ("availableReservations" in query) {
+        meals.where('max_reservations', '>' , '0');
       }
-    }
-    if (limit) {
-      if (!Number.isInteger(parseInt(limit))) {
-        response.statusCode = 400;
-        response.json({ "message": "bad input" });
-        return;
-      } else {
-        meals = meals.slice(0, limit)
+      if ("title" in query) {
+        meals.where({ title: query.title })
       }
+      if ("createdAfter" in query) {
+        if (!Date.parse(query.createdAfter)) {
+          response.statusCode = 400;
+          response.json({ "message": "bad input" });
+          return;
+        } else {
+          meals.where('created_date', '>', query.createdAfter);
+        }
+      }
+      if ("limit" in query) {
+        if (!Number.isInteger(parseInt(query.limit))) {
+          response.statusCode = 400;
+          response.json({ "message": "bad input" });
+          return;
+        } else {
+          meals.limit(query.limit)
+        }
+      }
+      return meals.select()
     }
     response.json(meals)
-    // Only specific number of meals with a specific max price // api/meals?limit=4&maxPrice=90
-    // } else if (limit && maxPrice) {
-    //   if (!Number.isInteger(parseInt(limit)) || !Number.isInteger(parseInt(maxPrice))) {
-    //     response.statusCode = 400;
-    //     response.json({ "message": "bad input" });
-    //     return;
-    //   } else {
-    //     const meal = await knex("meals").select("*").where('price', '<', maxPrice).limit(limit);
-    //     response.send(meal)
-    //   }
-    // // Get meals that has a price smaller than maxPrice // api/meals?maxPrice=90
-    // } else if (maxPrice) {
-    //   if (!Number.isInteger(parseInt(maxPrice))) {
-    //     response.statusCode = 400;
-    //     response.json({ "message": "bad input" });
-    //     return;
-    //   } else {
-    //     const meal = await knex("meals").select("*").where('price', '<', maxPrice);
-    //     response.json(meal)
-    //   }
-    //   // Get meals that still has available reservations // api/meals?availableReservations=true
-    // } else if (availableReservations) {
-    //   const meal = await knex("meals").select("*").where('max_reservations', '>' , '0');
-    //   response.json(meal)
-    //   // Get meals that partially match a title // api/meals?title=Indian%20platter
-    // } else if (title) {
-    //   const meal = await knex("meals").select("*").where({ title: title });
-    //   response.json(meal)
-    // // Get meals that has been created after the date // api/meals?createdAfter=2019-04-05
-    // } else if (createdAfter) {
-    //   if (!Date.parse(createdAfter)) {
-    //     response.statusCode = 400;
-    //     response.json({ "message": "bad input" });
-    //     return;
-    //   } else {
-    //     const meal = await knex("meals").select("*").where('created_date', '>', createdAfter);
-    //     response.json(meal)
-    //   }
-    // // Only specific number of meals // api/meals?limit=4
-    // } else if (limit) {
-    //   if (!Number.isInteger(parseInt(limit))) {
-    //     response.statusCode = 400;
-    //     response.json({ "message": "bad input" });
-    //     return;
-    //   } else {
-    //     const meal = await knex("meals").select("*").limit(limit);
-    //     response.send(meal)
-    //   }
-    // // Returns all meals // GET api/meals/
-    // } else {
-    //   response.send(meals);
-    // }
   } catch (error) {
     throw error;
   }
